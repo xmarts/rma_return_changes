9cbb67d23c2fefb340381a7f5a6dd56f6af34cc9
luis <angelchicles97@gmail.com>
2019-02-22 10:21:33 -0600

    .

--- rma_devo/__manifest__.py
+++ rma_devo/__manifest__.py
@@ -22,7 +22,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','sale','delivery','crm', 'purchase','rma_original'],
+    'depends': ['base','sale','delivery','crm', 'purchase','rma_ept'],
 
     # always loaded
     'data': [
